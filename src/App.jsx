1732e8a19bfcac339574922cde128f8e98eca6a0
huahuaccapa <huahuaccapa1119@gmail.com>
2025-04-30 00:17:28 -0500

    feat: actualizo estructura de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardLayout from '@/routes/dashboardlayout';
 import { TableDemo } from './pages/panels/Administrador/Cliente/Components/TablesDemo';
+import Login from "./pages/general/login/components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/general/login" element={<Login />} />
         <Route path="/" element={<DashboardLayout />}>
           <Route path="Clientes" element={<TableDemo />} />
           {/* Agrega más rutas aquí si quieres */}
