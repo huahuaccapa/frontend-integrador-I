2c6d8bdd2a60c34298814ddf1977659bf7c17106
huahuaccapa <huahuaccapa1119@gmail.com>
2025-05-02 02:39:15 -0500

    configuracion

---
+++ src/services/authService.js
@@ -0,0 +1,13 @@
+export async function login(username, password) {
+    const response = await fetch('http://localhost:8080/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+  
+    const data = await response.text(); // backend devuelve texto plano
+    return data;
+  }
+  
