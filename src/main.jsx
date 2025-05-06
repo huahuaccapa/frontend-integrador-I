638e4b8a0afb65684c6c25ed195ad61737950727
Alheni <114592057+Alheni@users.noreply.github.com>
2025-05-06 15:59:14 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // Si tienes estilos globales
+import './index.css' // Si tienes estilos globales s
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
