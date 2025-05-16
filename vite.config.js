d5cf95228407f4a095794671785aa5fd7fb31ee9
huahuaccapa <huahuaccapa1119@gmail.com>
2025-05-16 18:38:22 -0500

    Modifica vite.config.js para ajustes en el entorno de desarrollo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
